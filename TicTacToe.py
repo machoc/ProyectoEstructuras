0df71497feb8be2a79d95f5a58e1b8070dcb6344
machoc <luiscastaing06@hotmail.com>
2016-11-12 00:27:14 -0600

    arreglado

--- TicTacToe.py
+++ TicTacToe.py
@@ -3,6 +3,6 @@ import platform
 
 platform.system() == "Linux":
 	os.system ("git clone https://github.com/machoc/ProyectoEstructuras")
-	os.system ("g++ -std=c++14 ProyectoEstructuras/archivoshcpp/*.cpp gato/archivoshcpp/*.h -o sfml-app -lsfml-graphics -lsfml-window -lsfml-system")
+	os.system ("g++ -std=c++14 ProyectoEstructuras/archivoshcpp/*.cpp ProyectoEstructuras/archivoshcpp/*.h -o sfml-app -lsfml-graphics -lsfml-window -lsfml-system")
 	os.system ("cp -r ProyectoEstructuras/archivoshcpp/imagenesJuego .")
 	os.system ("./sfml-app")
